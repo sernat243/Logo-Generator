cae058ca403b1aaf3846b1399adfec36360a2eca
Gabriel <sernat243@gmail.com>
2023-08-16 12:02:58 -0600

    created questions

--- logoGenerator.js
+++ logoGenerator.js
@@ -1,3 +1,23 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const { Logo } = require('./logoUtils.js'); // I'll create this file later
+
+const questions = [
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Choose text color',
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape',
+        choices: ['circle', 'triangle', 'square'],
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Choose shape color',
+    },
+];
+
