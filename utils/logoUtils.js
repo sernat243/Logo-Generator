1a2046cbfbc9cf730fd7ae6fc7c32e815324e232
Gabriel <sernat243@gmail.com>
2023-08-16 12:21:37 -0600

    created Logo constructor class and exported it

---
+++ utils/logoUtils.js
@@ -0,0 +1,17 @@
+class Logo {
+    constructor(textColor, shape, shapeColor) {
+        this.textColor = textColor;
+        this.shape = shape;
+        this.shapeColor = shapeColor;
+    }
+
+    generateSVG() {
+        const svgTemplate = `
+        `;
+        return svgTemplate;
+    }
+};
+
+module.exports = {
+    Logo,
+};
